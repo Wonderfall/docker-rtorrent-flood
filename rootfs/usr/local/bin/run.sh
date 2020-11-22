43118efda4948bef1ddd034a284ef0af250763d9
Rémy Lescallier <lescallier1@gmail.com>
2020-11-22 05:51:06 +0100

    Update to the last flood version (4.1.2) and optimize start speed

--- rootfs/usr/local/bin/run.sh
+++ rootfs/usr/local/bin/run.sh
@@ -6,7 +6,7 @@ mkdir -p /data/.session
 
 rm -f /data/.session/rtorrent.lock
 
-chown -R $UID:$GID /data /home/torrent /tmp /usr/flood /flood-db /etc/s6.d
+chown -R $UID:$GID /data /home/torrent /tmp /usr/flood/dist /flood-db /etc/s6.d
 
 if [ ${RTORRENT_SOCK} = "false" ]; then
     sed -i -e 's|^scgi_local.*$|scgi_port = 0.0.0.0:5000|' /home/torrent/.rtorrent.rc
